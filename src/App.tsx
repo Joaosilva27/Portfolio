f2c87d77357fd7d76a013756d2ad8bfa6485c173
João Silva <joaosilva7875@gmail.com>
2025-03-02 18:52:57 +0100

    portfolio nav & css

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-xl">
-        João Silva: Bringing Ideas to Life Through Frontend Development
-      </h1>
+    <div className="flex flex-col items-center lg:w-5/6 m-5 lg:m-0 lg:mt-4 ">
+      <nav>
+        <h1 className="text-2xl font-bold">
+          João Silva: Bringing Ideas to Life Through Frontend Development.
+        </h1>
+        <div className="appearance-none mt-4">
+          <a className="mr-2">Projects</a>
+          <a className="mr-2">LinkedIn</a>
+          <a className="mr-2">Github</a>
+          <a>Contact</a>
+        </div>
+      </nav>
     </div>
   );
 }
