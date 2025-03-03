0a15c5a0d668fd5552b9c450b1b9bef355ad8eef
João Silva <joaosilva7875@gmail.com>
2025-03-03 13:07:25 +0100

    mobile responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@ function App() {
   const modelPath = "/models/maxwell.glb"; // Correct path for models inside the public folder
 
   return (
-    <div className="flex flex-col items-center lg:w-5/6 m-5 lg:m-0 lg:mt-4 ">
-      <nav>
-        <h1 className="text-2xl font-bold">
+    <div className="flex flex-col lg:w-5/6 m-5 lg:m-0 lg:mt-4 ">
+      <nav className="w-full">
+        <h1 className="text-2xl font-bold text-center ">
           João Silva: Bringing Ideas to Life Through Frontend Development.
         </h1>
-        <div className="appearance-none mt-4">
+        <div className="mt-4 flex flex-wrap justify-center gap-4">
           <a className="mr-2">Projects</a>
           <a
             href="https://www.linkedin.com/in/jo%C3%A3o-silva-8992b4221/"
@@ -34,7 +34,7 @@ function App() {
       </nav>
 
       {/* 3D model section */}
-      <div className="canvas-container flex justify-center items-center">
+      <div className="canvas-container flex items-center w-full max-w-full">
         {/* Set size for the 3D Canvas */}
         <CharacterScene modelPath={modelPath} />
       </div>
