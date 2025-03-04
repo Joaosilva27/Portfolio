c51285b37135c6784f6c7d618e43babf359876eb
João Silva <joaosilva7875@gmail.com>
2025-03-04 22:27:00 +0100

    added tobimasu music project

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -2,19 +2,21 @@ import GithubIcon from "./images/github.png";
 import GroceryListImage from "./images/groceryList.png";
 import PokemonTcgImage from "./images/pokemonTCG.png";
 import SmartbarImage from "./images/smartbar.png";
+import TobimasuImage from "./images/tobimasu.png";
 
 const ProjectsPage = () => {
   return (
     <div className="flex flex-col items-center pt-8 md:pt-10 min-h-screen px-4">
       <div className="flex items-center mb-6 md:mb-8">
-        <span className="font-bold text-base md:text-lg bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
-          View all my projects:
-        </span>
         <a
+          className="flex"
           href="https://github.com/Joaosilva27?tab=repositories"
           target="_blank"
           rel="noopener noreferrer"
         >
+          <span className="font-bold text-base md:text-lg bg-gradient-to-r from-purple-600 to-pink-600 bg-clip-text text-transparent">
+            View all my projects:
+          </span>
           <img
             src={GithubIcon}
             className="h-5 w-5 ml-2 animate-bounce"
@@ -22,6 +24,7 @@ const ProjectsPage = () => {
           />
         </a>
       </div>
+
       <div className="text-center flex flex-col md:flex-row items-center gap-6 mb-12 md:mb-14">
         <div className="flex flex-col justify-center">
           <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r text-green-400 bg-clip-text">
@@ -49,7 +52,8 @@ const ProjectsPage = () => {
           alt="Grocery List"
         />
       </div>
-      <div className="text-center flex flex-col md:flex-row items-center gap-6 mb-20 mt-25">
+
+      <div className="text-center flex flex-col md:flex-row items-center gap-6 mb-30 mt-25">
         <div className="flex flex-col justify-center md:order-2">
           <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-blue-300 to-blue-200 bg-clip-text text-transparent">
             Pokémon TCG Tracker
@@ -73,6 +77,7 @@ const ProjectsPage = () => {
           alt="Pokémon TCG"
         />
       </div>
+
       <div className="text-center flex flex-col md:flex-row items-center gap-6 mb-8 mt-14">
         <div className="flex flex-col justify-center">
           <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-cyan-600 to-blue-600 bg-clip-text text-transparent">
@@ -96,6 +101,35 @@ const ProjectsPage = () => {
           className="w-140 mx-auto h-fit"
           alt="Smartbar Interface"
         />
+      </div>
+
+      <div className="text-center flex flex-col md:flex-row items-center gap-6 mb-8 mt-40">
+        <img
+          src={TobimasuImage}
+          className="order-2 md:order-1 w-140 mx-auto h-fit md:mt-0"
+          alt="Tobimasu Music"
+        />
+        <div className="flex flex-col justify-center order-1 md:order-2">
+          <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold bg-gradient-to-r from-green-800 to-green-500 bg-clip-text text-transparent">
+            Tobimasu Music
+          </h3>
+          <p className="text-base md:text-lg text-gray-600 font-semibold mt-2 md:mt-3">
+            Music discovery platform with millions of tracks and albums.
+          </p>
+          <p className="text-sm text-gray-500 mt-2">
+            Search and organize music collections, track favorite albums, and
+            explore detailed information.
+          </p>
+          <a
+            href="https://github.com/Joaosilva27/Tobimasu-Music?tab=readme-ov-file"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="text-xs text-green-800 font-semibold mt-3">
+              Read more about it
+            </span>
+          </a>
+        </div>
       </div>
     </div>
   );
