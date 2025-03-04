1faeb39da9bc038de8b5a98d94a362488d50ccc0
João Silva <joaosilva7875@gmail.com>
2025-03-04 14:26:42 +0100

    grocerylist project

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -1,14 +1,32 @@
-import GithubIcon from "./images/githubIcon(1).png";
+import GithubIcon from "./images/github.png";
+import GroceryListImage from "./images/groceryList.png";
 
 const ProjectsPage = () => {
   return (
-    <div>
-      <div className="flex">
-        <span>You can view all my projects here:</span>
-        <img src={GithubIcon} className="h-5 w-5 object-contain" />
+    <div className="flex flex-col justify-center items-center mt-4">
+      <div className="flex justify-center items-center">
+        <span className="font-semibold">View all my projects here:</span>
+        <a
+          href="https://github.com/Joaosilva27?tab=repositories"
+          target="_blank"
+        >
+          <img
+            src={GithubIcon}
+            className="h-4 w-4 object-contain ml-1.5 animate-bounce"
+          />
+        </a>
       </div>
-      <div className="flex flex-col">
-        <span>Online Grocery List</span>
+      <div className="flex flex-col items-center text-center mt-10 pl-2 pr-2">
+        <h3 className="text-2xl font-bold mb-1">Online Grocery List</h3>
+        <span>
+          A real-time online collaborative grocery list application. Create,
+          share, and manage shopping lists with your household, partner or
+          friends.
+        </span>
+        <img
+          src={GroceryListImage}
+          className="object-contain h-100 w-80 mt-6"
+        />
       </div>
     </div>
   );
