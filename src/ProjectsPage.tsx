6e8ef17615abe9aa1b729139fc834465fbba9bee
João Silva <joaosilva7875@gmail.com>
2025-03-04 18:30:44 +0100

    projectsPage pokemon tcg

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -1,11 +1,14 @@
 import GithubIcon from "./images/github.png";
 import GroceryListImage from "./images/groceryList.png";
+import PokemonTcgImage from "./images/pokemonTCG.png";
 
 const ProjectsPage = () => {
   return (
-    <div className="flex flex-col text-gray-400 items-center pt-10 bg-gradient-to-r h-dvh from-blue-50 via-purple-50 to-pink-50">
+    <div className="flex flex-col items-center pt-10 bg-gradient-to-r  min-h-screen">
       <div className="flex items-center mb-6">
-        <span className="font-bold text-lg ">View all my projects:</span>
+        <span className="font-bold text-lg bg-gradient-to-r  bg-clip-text ">
+          View all my projects:
+        </span>
         <a
           href="https://github.com/Joaosilva27?tab=repositories"
           target="_blank"
@@ -18,24 +21,44 @@ const ProjectsPage = () => {
           />
         </a>
       </div>
-      <div className="text-center flex flex-wrap mb-8">
+
+      <div className="text-center flex flex-wrap mb-14">
         <div className="flex flex-col justify-center">
-          <h3 className="text-3xl font-bold lg:text-4xl">
+          <h3 className="text-3xl font-bold lg:text-4xl bg-gradient-to-r text-purple-400 bg-clip-text ">
             Online Grocery List
           </h3>
-          <p className="text-sm text-gray-700 font-semibold mt-2 lg:text-lg">
+          <p className="text-lg text-gray-600 font-semibold mt-2 lg:text-lg">
             A real-time online collaborative grocery list application.
           </p>
-          <p className="text-sm text-gray-700 font-semibold">
+          <p className="text-sm text-gray-500 font-semibold">
             Create, share, and manage shopping lists with your household,
             partner or friends.
           </p>
         </div>
-
         <img
           src={GroceryListImage}
           className="mt-4 max-w-xs mx-auto"
           alt="Grocery List"
+        />
+      </div>
+
+      <div className="text-center flex flex-wrap mb-8">
+        <div className="flex flex-col justify-center">
+          <h3 className="text-3xl font-bold lg:text-4xl bg-gradient-to-r from-cyan-600 to-blue-600 bg-clip-text text-transparent">
+            Pokémon TCG Tracker
+          </h3>
+          <p className="text-lg text-gray-600 font-semibold mt-2 lg:text-lg">
+            A real-time online collaborative grocery list application.
+          </p>
+          <p className="text-sm text-gray-500 font-semibold">
+            Create, share, and manage shopping lists with your household,
+            partner or friends.
+          </p>
+        </div>
+        <img
+          src={PokemonTcgImage}
+          className="mt-4 max-w-xs mx-auto"
+          alt="Pokémon TCG"
         />
       </div>
     </div>
