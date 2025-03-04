a8699022de33f281e690c0f27b5e694a04bc3a46
João Silva <joaosilva7875@gmail.com>
2025-03-04 14:58:49 +0100

    projectsPage prettier css

--- src/ProjectsPage.tsx
+++ src/ProjectsPage.tsx
@@ -3,29 +3,39 @@ import GroceryListImage from "./images/groceryList.png";
 
 const ProjectsPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center mt-4">
-      <div className="flex justify-center items-center">
-        <span className="font-semibold">View all my projects here:</span>
+    <div className="flex flex-col text-gray-400 items-center pt-10 bg-gradient-to-r h-dvh from-blue-50 via-purple-50 to-pink-50">
+      <div className="flex items-center mb-6">
+        <span className="font-bold text-lg ">View all my projects:</span>
         <a
           href="https://github.com/Joaosilva27?tab=repositories"
           target="_blank"
+          rel="noopener noreferrer"
         >
           <img
             src={GithubIcon}
-            className="h-4 w-4 object-contain ml-1.5 animate-bounce"
+            className="h-5 w-5 ml-2 animate-bounce"
+            alt="Github"
           />
         </a>
       </div>
-      <div className="flex flex-col items-center text-center mt-10 pl-2 pr-2">
-        <h3 className="text-2xl font-bold mb-1">Online Grocery List</h3>
-        <span>
-          A real-time online collaborative grocery list application. Create,
-          share, and manage shopping lists with your household, partner or
-          friends.
-        </span>
+      <div className="text-center flex flex-wrap mb-8">
+        <div className="flex flex-col justify-center">
+          <h3 className="text-3xl font-bold lg:text-4xl">
+            Online Grocery List
+          </h3>
+          <p className="text-sm text-gray-700 font-semibold mt-2 lg:text-lg">
+            A real-time online collaborative grocery list application.
+          </p>
+          <p className="text-sm text-gray-700 font-semibold">
+            Create, share, and manage shopping lists with your household,
+            partner or friends.
+          </p>
+        </div>
+
         <img
           src={GroceryListImage}
-          className="object-contain h-100 w-80 mt-6"
+          className="mt-4 max-w-xs mx-auto"
+          alt="Grocery List"
         />
       </div>
     </div>
