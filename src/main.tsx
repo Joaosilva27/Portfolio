de9ce3809599dd90dc244e330630412c85de1d77
Joaosilva27 <joaosilva7875@gmail.com>
2025-04-25 12:54:40 +0200

    removed all images & code cleanup
    * removed all project images due to them being unused - only using videos to showcase projects from now on

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router, Route, Routes } from "react-router"; // Use 'react-router-dom' instead of 'react-router'
+import { BrowserRouter as Router, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import ProjectsPage from "./ProjectsPage.tsx";
@@ -9,8 +9,8 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path='/' element={<App />} />
+        <Route path='/projects' element={<ProjectsPage />} /> {/* unused for now */}
       </Routes>
     </Router>
   </StrictMode>
